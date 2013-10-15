8981fe2caf28b589d432270f4030bf0bf0612432
Jon Schlinkert <dev@sellside.com>
2013-10-14 20:18:56 -0400

    refactored

--- tasks/lib/utils.js
+++ tasks/lib/utils.js
@@ -1,5 +1,5 @@
 /*!
- * Upstage Component Builder: Prettify
+ * Upstage Component Builder: Reverse
  * http://github.com/upstage/upstage
  *
  * Copyright 2013 Sellside, Inc.
@@ -12,19 +12,46 @@
 var path = require('path');
 
 
-var padcomments = function(str) {
-  return str.replace(/(\s*<!--)/g, '\n$1');
-}
-
 /**
  * Prettify HTML
  */
-var prettify = function(src) {
-  return require('js-prettify').html(src, {
+var prettify = function(str) {
+  return require('js-prettify').html(str, {
     indent_size: 2,
     indent_inner_html: true,
     unformatted: ['code', 'pre', 'em', 'strong']
-  }).replace(/(\r\n|\n\r|\n|\r){2,}/g, '\n').replace(/<\/a>\s*<\/li>/g, '</a> </li>');
+  });
+};
+
+exports.stripWhitespace = function(str) {
+  return str.replace(/^\s*/, '').replace(/\s*$/, '');
+};
+
+var collapseli = function(str) {
+  return str.replace(/<\/a>\s*<\/li>/g, '</a> </li>');
+};
+
+// Add a newline above each code comment.
+var padcomments = function(str) {
+  return str.replace(/(\s*<!--)/g, '\n$1');
+};
+
+// Remove superfluous whitespace
+var condense = function(str) {
+  return str.replace(/(\r\n|\n\r|\n|\r){2,}/g, '\n');
+};
+
+// If one exists, lop the first empty line off the top of a file.
+var shave = function(str) {
+  return str.replace(/^\s*/g, '');
+};
+
+var fixClosingComments = function(str) {
+  return str.replace(/\s*(<!--\s*\/.+)/g, '$1');
+};
+
+var padComponents = function(str) {
+  return str.replace(/^<(?:(?!\/))/gm, '\n<');
 };
 
 /**
@@ -33,16 +60,20 @@ var prettify = function(src) {
  * @param  {String} separator The separator to use between sections of joined content.
  * @return {String}     
  */
-exports.format = function(src, separator) {
-  separator = separator || '';
-  return padcomments(separator + prettify(src)).replace(/^\s*/g, '');
+exports.format = function(src, sep) {
+  src = prettify(src);
+  src = condense(src);
+  src = padcomments(src);
+  src = padComponents(src, sep);
+  src = shave(src)
+  return fixClosingComments(src);
 };
 
 /**
  * Default separator
  */
 exports.sep = function(str) {
-  return '<!-- ' + str + '-->\n';
+  return '<!--  ' + str + '  -->\n';
 }
 
 /**
@@ -53,7 +84,7 @@ exports.sep = function(str) {
  * @return {Array}       
  */
 exports.patternArray = function(regex, src) {
-  src = require('grunt').file.read(src);
+  // src = require('grunt').file.read(src);
   var match;
   var matches = [];
   while (match = regex.exec(src)) {
@@ -61,6 +92,7 @@ exports.patternArray = function(regex, src) {
   }
   return matches;
 };
+
 // basename, excluding extension
 exports.base = function(filepath) { 
   return path.basename(filepath, path.extname(filepath));
--- tasks/reverse.js
+++ tasks/reverse.js
@@ -7,28 +7,47 @@
  * http://opensource.org/licenses/MIT
  */
 
+'use strict';
+
 // Node.js
 var path = require('path');
-var fs = require('fs');
+var fs   = require('fs');
+
 
 // node_modules
 var cheerio = require('cheerio')
-var _ = require('grunt').util._;
+var _       = require('grunt').util._;
+
 
 // Local utils
-var Utils = require('./lib/utils');
+var Utils = require('./lib/utils.js');
+
 
 module.exports = function (grunt) {
-  'use strict';
 
-  var config = grunt.file.readJSON('package.json') || {};
 
   grunt.registerMultiTask('reverse', 'Deconstruct your HTML into components.', function () {
-    
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({});
 
-    this.files.forEach(function(fp) {
+    var cb = this.async();
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      tags: []
+    });
+
+    var files = this.files;
+
+    if (!files.length) {
+      grunt.log.writeln('No existing files in this target.');
+      return cb();
+    }
+
+    var data = [];
+
+    grunt.util.async.forEachSeries(files, function(fp, callback) {
+      var dest = fp.dest;
+      var src = fp.dest;
+
+      // Concat banner + specified files + footer.
       fp.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
@@ -37,29 +56,91 @@ module.exports = function (grunt) {
         } else {
           return true;
         }
-      }).map(function(src) {
-        var base = path.basename(src, path.extname(src));
- 
-         // Load the HTML
-        var $ = cheerio.load(grunt.file.read(src));
+      }).map(function(file) {
 
-        // Extract elements from DOM
-        _(options.tags).map(function(tagname) {
-          var content = Utils.sep(tagname) + $(tagname).map(function(i, el) {
-            return $(this).html();
-          }).join(Utils.sep(tagname));
-          grunt.file.write(path.join(fp.dest, base + '-' + tagname + '.html'), Utils.format(content, ''));
-        });
 
-        // Extract components from DOM
-        _(options.components).map(function(componentname) {
-          var content = Utils.sep(componentname) + $(options.parent + ' .' + componentname).map(function(i, el) {
-            return $(this).parent().html();
-          }).join(Utils.sep(componentname));
-          grunt.file.write(path.join(fp.dest, base, componentname + '.html'), Utils.format(content, ''));
+        var $ = cheerio.load(grunt.file.read(file));
+
+        var filename = path.basename(file);
+        var basename = path.basename(file, path.extname(file));
+        var dirname  = path.dirname(file);
+
+
+        /**
+         * Components
+         */
+        if(!options.elements) {
+          var components = [];
+          var parent = '.' + (options.parent || 'bs-example');
+
+          $(parent).each(function (i, elem, cb) {
+            components[i] = $(this).html();
+
+            var num = 0;
+            var totalComponents = components.length;
+            if (num < totalComponents) {
+
+              // Search for the first class name of each component
+              var name = $(components[i]).filter(function (i, el) {
+                return $(this).attr('class') !== undefined;
+              }).attr('class');
+
+              // Slugify, then remove duplicate name segments.
+              // "nav-nav-tabs" => "nav-tabs"
+              var slug = ((_.slugify(name) + i) || (parent + i) || 'element');
+              slug = _.unique(slug.split('-')).join('-');
+
+              // Prettify HTML and add a separator between any sibling components.
+              var result = Utils.format(components[i], Utils.sep(slug));
+
+              // Write the component to disk.
+              grunt.file.write(path.join(dest, basename, slug + '.' + options.ext), result);
+            } else {
+              if (cb) {
+                cb(null, true);
+              }
+              return true;
+            }
+          });
+          grunt.log.ok('Components generated in'.yellow, path.join(dest, basename));
+        }
+
+
+        // Attempt to extract reasonable file names.
+        var name;
+        if(basename === 'index') {
+          name = path.basename(dirname);
+        } else {
+          name = basename;
+        }
+
+
+        // var destpath = path.join(dest, basename, name + '.html');
+
+        // grunt.file.write(destpath, Utils.format($(this).html(), Utils.sep(name)));
+        _(options.tags).map(function(tag) {
+          $(tag).each(function(i, element) {
+
+            var title = $(this).text();
+
+            // Extract components from DOM
+            var metadata = {
+              filename: name,
+              src: file,
+              dest: dest,
+              title: Utils.stripWhitespace(title)
+            };
+
+            // Push meta-data into repos array
+            data.push(metadata);
+            grunt.file.write(path.join(path.dirname(files.dest), options.tags + '.json'), JSON.stringify(data, null, 2));
+          });
         });
+        console.log(data)
 
       });
+    }, function (err) {
+      callback(!err);
     });
   });
 };
